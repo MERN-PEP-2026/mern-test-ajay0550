f0f2bb4563115c35aa23c2fcdccd3bf0d264eeb7
ajay0550 <ajaygundu03@gmail.com>
2026-02-27 13:26:40 +0530

    connected register page to backend

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,5 +1,61 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+
 function Register() {
-  return <h2>Register Page</h2>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:5000/api/auth/register", {
+        name,
+        email,
+        password
+      });
+
+      alert("Registered successfully");
+      navigate("/");
+    } catch (error) {
+      alert(error.response?.data?.message || "Error registering");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Register</button>
+      </form>
+
+      <Link to="/">Go to Login</Link>
+    </div>
+  );
 }
 
 export default Register;
