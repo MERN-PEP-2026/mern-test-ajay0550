66c5534565e39cd3010e05a64d4d0eb4b08de0de
ajay0550 <ajaygundu03@gmail.com>
2026-02-27 13:27:03 +0530

    connected login page and stored jwt token

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,54 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+
 function Login() {
-  return <h2>Login Page</h2>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        { email, password }
+      );
+
+      localStorage.setItem("token", res.data.token);
+
+      navigate("/dashboard");
+    } catch (error) {
+      alert(error.response?.data?.message || "Login failed");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Login</button>
+      </form>
+
+      <Link to="/register">Go to Register</Link>
+    </div>
+  );
 }
 
 export default Login;
